23cfe20fffa5fbfa29eddb7ff2aac3221c84e908
scot forshaw <scot.forshaw@gmail.com>
2019-04-25 09:42:51 +0000

    index.php edited online with Bitbucket

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ Author Scot D Forshaw
 </head>
 <body>
 <h1>TQNN 1 SDK Examples</h1>
+<p>A set of example applications that perform the basic tasks of registration and validation on TQNN authAPI system</p>
 <p><a href="tqnn_v1_api_sdk/examples/registerID.html?">registerID</a></p>
 <p><a href="tqnn_v1_api_sdk/examples/authID.html?">authID</a></p>
 <p><a href="tqnn_v1_api_sdk/examples/updateID.html?">updateID</a></p>
