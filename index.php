c2407d812c44d66b0203f480dc7076f9af63cfd5
scot forshaw <scot.forshaw@gmail.com>
2019-04-25 06:14:08 +0000

    index.php edited online with Bitbucket

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ Author Scot D Forshaw
 <head>
 </head>
 <body>
-<a href="registerID.html?">registerID</a> | <a href="authID.html?">authID</a> | <a href="updateID.html?">updateID</a> | <a href="revokeID.html?">revokeID</a>
+<a href="tqnn_v1_api_sdk/examples/registerID.html?">registerID</a> | <a href="tqnn_v1_api_sdk/examples/authID.html?">authID</a> | <a href="tqnn_v1_api_sdk/examples/updateID.html?">updateID</a> | <a href="tqnn_v1_api_sdk/examples/revokeID.html?">revokeID</a>
 <h1>TQNN 1 SDK Examples</h1>
 </body>
 </html>
