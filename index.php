08e323118dabceb32dd9365aad29b25971d11798
scot forshaw <scot.forshaw@gmail.com>
2019-04-25 06:10:18 +0000

    index.php edited online with Bitbucket

--- index.php
+++ index.php
@@ -8,7 +8,13 @@ Author Scot D Forshaw
 */
 
 
-echo "Hello World";
-
 
 ?>
+<html>
+<head>
+</head>
+<body>
+<a href="registerID.html?">registerID</a> | <a href="authID.html?">authID</a> | <a href="updateID.html?">updateID</a> | <a href="revokeID.html?">revokeID</a>
+<h1>TQNN 1 SDK Examples</h1>
+</body>
+</html>
