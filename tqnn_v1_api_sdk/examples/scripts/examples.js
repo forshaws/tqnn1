bfdcf2e35b5d77358bc5a1da2581aee46712f73d
scot.forshaw <scot.forshaw@toridion.com>
2019-05-26 07:54:59 +0100

    search store

--- tqnn_v1_api_sdk/examples/scripts/examples.js
+++ tqnn_v1_api_sdk/examples/scripts/examples.js
@@ -213,3 +213,106 @@ function revokeIDExample(){
 	});
 
 }
+
+function searchPatternExample(){
+
+	var pattern=document.getElementById('pattern').value;
+	var return_filelist=document.getElementById('filelist').checked;
+	var filelist=0;
+	if(return_filelist){
+	var filelist=1;
+	}
+	
+	if(!window.navigator.onLine){
+		alert("Cannot connect to the Internet. Check your connection and try again");
+		return false;		
+	}
+	
+	document.getElementById('action_button').disabled=true;
+	
+	prc=1;
+
+	$.post("../searchPattern.php", { pattern:pattern, filelist:filelist, dataset:"cache"},
+	function(data,status) {
+	
+		//alert(data);
+		var json=JSON.parse(data);
+
+		if(json.error_message != "" && json.error_message != undefined  ){
+
+			var str = JSON.stringify(json, null, 2);
+			document.getElementById('api_response').value=str;
+			return;
+			
+		}
+		else{
+
+			var str = JSON.stringify(json, null, 2)
+			document.getElementById('api_response').value=str;		
+		}
+
+	}).done(function() {
+		prc=0;
+		document.getElementById('action_button').disabled=false;
+  
+	  }).error(function() {
+		prc=0;
+		
+		document.getElementById('action_button').disabled=false;
+
+	});
+
+
+}
+
+function storePatternExample(){
+
+	var pattern=document.getElementById('pattern').value;
+
+	
+	if(!window.navigator.onLine){
+		alert("Cannot connect to the Internet. Check your connection and try again");
+		return false;		
+	}
+	
+	prc=1;
+	
+	document.getElementById('action_button').disabled=true;
+	
+	/*
+	An alternative to straightforward registration is to first test if the account already exists
+	if it does do not try to register it again.
+	*/
+
+	$.post("../storePattern.php",  { pattern:pattern, dataset:"cache"},
+	function(data,status) {
+	
+		
+		var json=JSON.parse(data);
+
+		if(json.error_message != "" && json.error_message != undefined  ){
+
+			var str = JSON.stringify(json, null, 2);
+			document.getElementById('api_response').value=str;
+			return;
+			
+		}
+		else{
+
+			var str = JSON.stringify(json, null, 2)
+			document.getElementById('api_response').value=str;		
+		}
+
+	}).done(function() {
+		prc=0;
+		
+		document.getElementById('action_button').disabled=false;
+  
+	  }).error(function() {
+		prc=0;
+		
+		document.getElementById('action_button').disabled=false;
+
+	});
+
+}
---
+++ tqnn_v1_api_sdk/storePattern.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+
+Author: Scott D Forshaw :: Visicom Scientific Software :: Copyright 2017 ARR
+
+Twitter: @softwareguru1
+email: scot.forshaw@gmail.com
+
+filename: examples/registerID.php
+description: Thin client script that communicates with the Toridion registerID API
+
+
+*/
+
+$tqnnAPIKEY="123456789";
+$tqnnAPISECRET="123456789S";
+
+//format a curl request and send it to the API
+date_default_timezone_set('GMT');
+$datestamp=date('Y-m-d H:i:s');
+$user_request_id="example";
+
+$field = array(
+	'tqnnAPIKEY' => urlencode($tqnnAPIKEY),
+	'tqnnAPISECRET' => urlencode($tqnnAPISECRET),
+	'user_request_id' => urlencode($user_request_id),
+	'pattern' => urlencode(isset($_POST['pattern'])?$_POST['pattern']:""),
+	'dataset' => urlencode(isset($_POST['dataset'])?$_POST['dataset']:""),
+	'mode' => urlencode(isset($_POST['mode'])?$_POST['mode']:""),
+	'log_results' => urlencode('1'));
+
+$fields_string="";
+foreach($field as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+rtrim($fields_string, '&');
+
+$url = "https://api.toridion.com/v1/storePattern"; 
+
+//open connection
+$ch = curl_init();
+
+//HEADER SET FOR LOCAL DEV ONLY - curl_header_host set in api_config
+if(isset($curl_header_host)) {
+	//curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_header_host);
+}
+
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($field));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_USERPWD, "wearecommit:password1!");
+//curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+//execute post
+$json = curl_exec($ch);
+
+
+//close connection
+curl_close($ch);
+
+echo $json;
+
+?>
