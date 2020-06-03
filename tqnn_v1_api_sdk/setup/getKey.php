be8ba1d8e395fbc8dfe66d1e30dbef693fc93c88
forshawscot <scot.forshaw@gmail.com>
2020-06-03 13:04:51 +0100

    update power 2

--- tqnn_v1_api_sdk/setup/getKey.php
+++ tqnn_v1_api_sdk/setup/getKey.php
@@ -58,6 +58,8 @@ curl_setopt($ch,CURLOPT_URL, $url);
 curl_setopt($ch,CURLOPT_POST, count($field));
 curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 //curl_setopt($ch, CURLOPT_USERPWD, "user:pass");
 //curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 
